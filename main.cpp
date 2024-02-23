47187ee2ae72393c34bea7163ad14da09ab490e4
rawatvansh0000 <32318738-rawatvansh0000@users.noreply.replit.com>
2024-02-23 13:40:00 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int arr[10], key, i;
+  cout << "Enter elements to make an array" << endl;
+  for (i = 0; i < 10; i++) {
+    cin >> arr[i];
+  }
+  cout << "Enter a number to search" << endl;
+  cin >> key;
+  for (i = 0; i < 10; i++) {
+    if (arr[i] == key) {
+      cout << "Number found at index number " << i << endl;
+      break;
+    }
+  }
+}
